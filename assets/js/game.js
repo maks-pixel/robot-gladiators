a2abc45eb9c39f9e7aafb0b16d665c6343a11414
Makda Girmay <makdagirmay4@gmail.com>
2021-09-07 18:21:29 -0400

    attack points

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,32 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robots name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+//You can also log multiple values at once like this 
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealthy = 50;
+var enemyAttack = 12;
+
+var fight = function(){
+    window.alert("Welcome to Robot Gladiators!");
+
+    //subtract playerAttack from enemyHealth and update enemyHealth
+    enemyHealth = enemyHealth - playerAttack;
+    //log result
+
+    console.log(
+        playerName + " attacked " + enemyName + "." + enemyName + " now has " + enemyHealth + " health remaining."
+    );
+
+    //subtract enemyAttack from playerHealth and update playerHealth
+
+    playerHealth = playerHealth - enemyAttack;
+    
+    //log result
+
+    enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+};
+
+fight();
